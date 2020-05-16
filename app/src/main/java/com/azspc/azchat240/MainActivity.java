b502d940fa2c4113376a9aedc481ce0004dd1de0
EVERYVERY-AZ218 <45176850+EVERYVERY-AZ218@users.noreply.github.com>
2020-05-17 00:21:38 +0300

    фыв

--- app/src/main/java/com/azspc/azchat240/MainActivity.java
+++ app/src/main/java/com/azspc/azchat240/MainActivity.java
@@ -5,8 +5,6 @@ import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-
 import androidx.annotation.RequiresApi;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
@@ -15,147 +13,135 @@ import androidx.recyclerview.widget.RecyclerView;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.StrictMode;
+import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.Toast;
 
 
 import java.io.File;
-import java.io.IOException;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
-import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
-    public static int language;
-    public static final String post_separator = "║";
-    public static final String post_data_separator = "│";
+    //public static int language;
+    public static final String sep_post = "║";
+    public static final String sep_part = "│";
     public static final String lang_separator = ";";
-    static final String url = "http://azsspc.moy.su/posts.txt";
+    static final String url = "https://raw.githubusercontent.com/AZsSPC/test/master/README.md";
     static final String saveName = "posts.txt";
     static final String saveTo = "/storage/emulated/0/AZsSPC/AZs240";
     private RecyclerView recyclerView;
-    boolean logged;
+    boolean isModerator = false;
+    boolean isMenuVisible = false;
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    protected void onResume() {
+        super.onResume();
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 0);
+        reloadPosts(null);
+    }
 
     @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        language = Locale.getDefault().getLanguage().equals("ua") ? 1 : 0;
-        logged = true;
+        //language = Locale.getDefault().getLanguage().equals("ua") ? 1 : 0;
+        isModerator = true;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        StrictMode.ThreadPolicy newPolicy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-        StrictMode.setThreadPolicy(newPolicy);
+        StrictMode.ThreadPolicy pol = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(pol);
         recyclerView = findViewById(R.id.tab_post);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        updatePosts();
-        setTitle(getString(R.string.tab_name).split(lang_separator)[language]);
-/*      FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (logged) {
-                    setContentView(R.layout.create_post);
-                } else {
-                    setContentView(R.layout.activity_login);
-                    TextView title = findViewById(R.id.login_about);
-                    title.setText(getString(R.string.login_info).split(lang_separator)[language]);
-
-                    Button button = findViewById(R.id.login);
-                    button.setText(getString(R.string.login_go).split(lang_separator)[language]);
-
-                    EditText editText = findViewById(R.id.username);
-                    editText.setHint(getString(R.string.login_name).split(lang_separator)[language]);
-
-                    EditText editText1 = findViewById(R.id.password);
-                    editText1.setHint(getString(R.string.login_id).split(lang_separator)[language]);
-
-                }
-            }
-        });*/
     }
 
     @RequiresApi(api = Build.VERSION_CODES.O)
     private ArrayList<Post> getInitialData() {
         ArrayList<Post> posts = new ArrayList<>();
-        for (String post : getPostsFromCloud().split(post_separator)) {
+        for (String post : getPostsFromCloud().split(sep_post)) {
             try {
-                String[] pre_p = post.split(post_data_separator);
+                String[] pre_p = post.split(sep_part);
                 posts.add(0, new Post(getResources(), pre_p[0], pre_p[1], pre_p[2]));
             } catch (Exception ignored) {
-                posts.add(0, new Post(getResources(), "Ошибка", "Ошибка чтения данных.", "-1"));
+                if (isModerator)
+                    posts.add(0, new Post(getResources(), "Помилка читання поста", "Весь вміст:\n\n" + post, "-1"));
             }
         }
         return posts;
     }
 
-    public File getAlbumStorageDir(String fileName) throws Exception {
-        File dir = new File(saveTo);
-        File file = new File(dir, fileName);
-        if (!file.exists()) {
-            dir.mkdirs();
-            file.createNewFile();
-        }
-        return file;
-    }
-
-    protected void onResume() {
-        super.onResume();
-        getWindow().getDecorView().setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
-        if (ContextCompat.checkSelfPermission(
-                this,
-                Manifest.permission.WRITE_EXTERNAL_STORAGE)
-                != PackageManager.PERMISSION_GRANTED)
-            ActivityCompat.requestPermissions(
-                    this,
-                    new String[]{
-                            Manifest.permission.WRITE_EXTERNAL_STORAGE},
-                    0);
-    }
-
     @RequiresApi(api = Build.VERSION_CODES.O)
-    public void updateCloudData(View v) {
-        updatePosts();
+    public void reloadPosts(View v) {
+        closeMenu();
+        recyclerView.setAdapter(new DataAdapter(this, getInitialData()));
     }
 
-    @RequiresApi(api = Build.VERSION_CODES.O)
-    public void updatePosts() {
-        try {
-            recyclerView.setAdapter(new DataAdapter(this, getInitialData()));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    public void menuFab(View v) {
+        findViewById(R.id.fab_men).setVisibility((isMenuVisible = !isMenuVisible) ? View.VISIBLE : View.INVISIBLE);
+    }
+
+    public void infoScreen(View v) {
+        closeMenu();
+    }
+
+    public void createPost(View v) {
+        closeMenu();
+    }
+
+    void closeMenu() {
+        findViewById(R.id.fab_men).setVisibility((isMenuVisible = false) ? View.VISIBLE : View.INVISIBLE);
     }
 
     @RequiresApi(api = Build.VERSION_CODES.O)
     String getPostsFromCloud() {
-        String content = "";
+        String anotherPosts = "";
         try {
-            Path path = Paths.get(getAlbumStorageDir(saveName).getPath());
-            Files.copy(new URL(url).openStream(), path, StandardCopyOption.REPLACE_EXISTING);
-            content = Files.lines(path).reduce("", (a, b) -> a + "\n" + b).substring(1);
-        } catch (Exception e) {
-            e.printStackTrace();
+            Path path = Paths.get(getPostFile(saveName).getPath());
+            try {
+                Files.copy(new URL(url).openStream(), path, StandardCopyOption.REPLACE_EXISTING);
+                Toast.makeText(getBaseContext(),
+                        "Розпочато оновлення постів, це може зайняти час.",
+                        Toast.LENGTH_LONG).show();
+            } catch (Exception ignored) {
+                Toast.makeText(getBaseContext(),
+                        "Погане підключення до інтернету, " +
+                                "будуть показані тільки раніше завантажені пости.",
+                        Toast.LENGTH_LONG).show();
+            }
+            anotherPosts = Files.lines(path).findFirst().orElse(null);
+        } catch (Exception ignored) {
         }
-        return "О приложении" + post_data_separator +
-                "Спасибо что установили это приложение, тепрь Вы можете следить за новостями нашей школы!\n\n" +
-                "В приложении есть несколько типо постов:\n" +
-                " • Серый - системный\n" +
-                " • Синий - тихий\n" +
-                " • Зеленый - обычный\n" +
-                " • Красный - срочный\n" +
-                "Тип поста ни на что не влияет кроме как быстрой классификации поста для пользователя. Проще говоря - что бы Вам было удобнее.\n\n" +
-                //"Что бы добавить пост нужно подтвердить свою личность, это можно сделать через приложение (в будущем)\n\n" +
-                "Приложение создано под руководством Олега Н. - нового президента школы\n\n" +
-                "Хорошей учебы и просто - удачи!" + post_data_separator +
-                "-1" + post_separator
-                + content;
+        return ("Про програму" + sep_part +
+                "Дякую що встановили цей додаток, " +
+                "теперь Ви можете слідкувати за новинами нашої школи!\n\n" +
+                "У додатку є кілька типів постів:\n" +
+                " • Сірий - системний\n" +
+                " • Синій - тихий\n" +
+                " • Зелений - звичайний\n" +
+                " • Червоний - терміновий\n\n" +
+                "Тип поста ні на що не впливає крім як швидкої класифікації" +
+                " поста для користувача. Простіше кажучи - що б Вам було зручніше.\n\n" +
+                "Додаток створено під керівництвом Олега Н. - нового президента школи.\n\n" +
+                "Гарного навчання та просто - удачі!" + sep_part +
+                "-1" + sep_post
+                + anotherPosts).replaceAll("\\\\n", "\n");
     }
+
+    public File getPostFile(String fileName) throws Exception {
+        File dir = new File(saveTo);
+        File file = new File(dir, fileName);
+        if (!file.exists()) if (dir.mkdirs() && file.createNewFile())
+            Log.i("created new file", "the post file has been recreated");
+        return file;
+    }
+
+
 }
